bea0d6e0fee803f87265e5bdb78233b745a3dcc3
Luke <luciusfow@gmail.com>
2022-04-14 22:40:40 -0400

    working form

--- src/components/LogForm.js
+++ src/components/LogForm.js
@@ -1,16 +1,36 @@
-
+import { useState } from 'react'
 function LogForm(){
+    const [ date, setDate] = useState("")
+    const [ time, setTime] = useState("")
+    const [ swell, setSwell] = useState("")
+    const [ note, setNote] = useState("")
+
+   function handleSubmit (e) {
+    e.preventDefault()
+    const post = { date, time, note, swell}
+    fetch('http://localhost:3001/surflogs', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(post)
+        }).then(() => {
+            console.log('new log added')
+        })
+    
+    }
+
+   
     return (
      <div className="logform">
         <h2>Enter a new surf report below!</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
             <label>date</label>
-                <input type="date" />
-                <label>Time </label>
-                <select>
-                    <option value="Morning">Small</option>
-                    <option value="Midday">Medium</option>
-                    <option value="Evening">Over Head</option>
+                <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+            
+            <label>Time </label>
+                <select value={time} onChange={(e) => setTime(e.target.value)}>
+                    <option value="Morning">Morning</option>
+                    <option value="Midday">Midday</option>
+                    <option value="Evening">Evening</option>
                 </select>
                 {/* <input type="time" id="stop" /> */}
             {/* <label>Stop</label>
@@ -18,18 +38,18 @@ function LogForm(){
 
             {/* <label>Session Length</label>
                 <input type="text" /> */}
-
-            <label>Session Notes</label>
-                <input type="length" id="duration" />
-
             <label>Swell Size </label>
-                <select>
+                <select value={swell} onChange={(e) => setSwell(e.target.value)} >
                     <option value="small">Small</option>
                     <option value="medium">Medium</option>
                     <option value="OverHead">Over Head</option>
                 </select>
-         <button> Add Session </button>
-        </form>
+
+            <label>Session Notes</label>
+                <input type="length" value={note} onChange={(e) => setNote(e.target.value)}/>
+
+                <button> Add Session </button>
+            </form>
     </div>
     )
 }
--- src/containers/Logs.js
+++ src/containers/Logs.js
@@ -16,11 +16,9 @@ return (
         {/* <h1>need to work on this ahh</h1> */}
         {logs.map((log) => (
         <div className="every-log" key={log.id}>
-            <h5> {`${log.startTime} - ${log.endTime}`}</h5>
-           
-            <h1> {log.sessionLength}</h1>
-            <h1> {log.surfSize}</h1>
-            <h2> {log.sessionNotes}</h2>
+            <h5> {`${log.date} - ${log.time}`}</h5>
+            <h1> {log.swell}</h1>
+            <h1> {log.note}</h1>
             </div>
         ))}
     </div>
