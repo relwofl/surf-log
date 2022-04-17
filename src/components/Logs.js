870966f8915ab3b03737ded48ed027c5f3a2dece
Luke <luciusfow@gmail.com>
2022-04-16 23:34:38 -0400

    props work

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,26 +1,31 @@
 // import React  from "react"
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import LogList from './LogList';
 
 function Logs(){
-    const [logs, setLogs] = useState([])
+    const [logs, setLogs] = useState([
+        {title: "lunch break refill", date: "2022-03-27", time: "Midday", note: " Went out and got worked!", swell: "Over Head", id:1},
+        {title: "lunch break refill", date: "2022-03-28", time: "Midday", note: "Surfing solo during lunch. Went out and got worked!", swell: "Over Head", id:3},
+        {title: "lunch break refill", date: "2022-03-29", time: "Morning", note: "yew!", swell: "Over Head", id:3}
 
-    useEffect(() => {
-        fetch('http://localhost:3001/surflogs')
-          .then(r => r.json())
-          .then(logs => setLogs(logs))
-          
-      }, []);
+    ])
+
+    // useEffect(() => {
+    //     fetch('http://localhost:3001/surflogs')
+    //       .then(r => r.json())
+    //       .then(logs => setLogs(logs))
+    //   }, []);
 
 return (
     <div className="Logs">
-        {/* <h1>need to work on this ahh</h1> */}
-        {logs.map((log) => (
-        <div className="every-log" key={log.id}>
+     {logs.map((log) => (
+        <div className="log-details" key={log.id}>
+            <h4> {log.title} </h4>
             <h5> {`${log.date} - ${log.time}`}</h5>
-            <h1> {log.swell}</h1>
-            <h1> {log.note}</h1>
+            <h5> {log.swell} </h5>
+            <p> {log.note}</p>
             </div>
-        ))}
+     ))}
     </div>
 )
 }
